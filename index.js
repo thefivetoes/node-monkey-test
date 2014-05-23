e8e66d00414e26a8b1971b58473b82b773cba795
thefivetoes <justin.smith@thundertech.com>
2014-05-23 14:34:23 -0400

    console log on get

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
   res.send('Hello World!?');
+  console.log('someone connected');
 });
 
 var port = Number(process.env.PORT || 5000);
