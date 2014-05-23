cb64063687db6451418a8addd896e168e92889d5
thefivetoes <justin.smith@thundertech.com>
2014-05-23 14:16:18 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+var http = require( 'http' );
+var port = Number(process.env.PORT || 5000);
+
+http.createServer( function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+} ).listen(port, '127.0.0.1');
