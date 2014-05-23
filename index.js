d0211fb494a9e98a225de1a5f102019e184a7799
thefivetoes <justin.smith@thundertech.com>
2014-05-23 14:33:00 -0400

    added express

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-var http = require( 'http' );
-var port = Number(process.env.PORT || 5000);
+// web.js
+var express = require("express");
+var logfmt = require("logfmt");
+var app = express();
 
-http.createServer( function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-} ).listen(port, '127.0.0.1');
+app.use(logfmt.requestLogger());
+
+app.get('/', function(req, res) {
+  res.send('Hello World!?');
+});
+
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
