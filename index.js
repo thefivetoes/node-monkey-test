331e88152f53ad373ca0e007667a400844a493b2
justin smith <justin.smith@thundertech.com>
2014-05-24 14:50:39 -0400

    added file storage for recent push

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // web.js
+var fs = require("fs");
 var express = require("express");
 var logfmt = require("logfmt");
 var bodyParser = require('body-parser');
@@ -17,19 +18,26 @@ app.get('/', function(req, res) {
     output += '<span>nothing to report. push some github webhooks to /github</span>';
   }
   res.send( output );
-  //res.send('latest push by <span><em>'+payload.pusher.name+'</em></span>');
-  //res.send('hey go ahead and push some github webhooks to /github.');
 });
 
 app.use(bodyParser());
 
 app.post('/github', function(request, response){
   payload = request.body;
+  fs.writeFile('recent-push.json', JSON.stringify(request.body), function (err) {
+    if (err) throw err;
+  });
   console.log(request.body);
   response.send(request.body);
 });
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
+  fs.readFile('recent-push.json', {'encoding':'utf8'}, function(err,data){
+    payload = data;
+    console.log(payload);
+  });
   console.log("Listening on " + port);
 });
+
+//curl -H "Content-Type: application/json" -d '{"username":"xyz","password":"xyz"}' http://localhost:5000/github
